ba2eb0d6723c7a9bd89ea7bdaf13d27d3db4541e
Priya R6 <priya.r6@ibm.com>
2023-03-26 19:54:07 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Posts } from "./postType";
+export default function App() {
+  const [postsList, setPosts] = useState<Posts>([]);
+  const [rowItems, setrowItems] = useState<Posts>([]);
+  const [rowPerPage, setrowPerPage] = useState<number>(10);
+  useEffect(() => {
+    async function fethdata() {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+      const data = await res.json();
+      setPosts(data);
+    }
+    fethdata();
+  }, []);
 
-function App() {
+  const onPageChange = (i: number) => {
+    const item = postsList.slice(i * rowPerPage, rowPerPage * (i + 1));
+    setrowItems(item);
+  };
+
+  useEffect(() => {
+    onPageChange(0);
+  }, [rowPerPage]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>userId</td>
+            <td>completed</td>
+            <td>title</td>
+          </tr>
+        </thead>
+        <tbody>
+          {postsList &&
+            rowItems.map((el) => {
+              return (
+                <tr>
+                  <td>{el.id}</td>
+                  <td>{el.userId}</td>
+                  <td>{el.completed}</td>
+                  <td>{el.title}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+
+      {Array(10)
+        .fill(null)
+        .map((el, idx) => {
+          return <button onClick={() => onPageChange(idx)}>{idx + 1}</button>;
+        })}
     </div>
   );
 }
-
-export default App;
---
+++ src/postType.ts
@@ -0,0 +1,8 @@
+export interface post {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+  
+  export type Posts = post[];
